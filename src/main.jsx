197ac80484a3b8110772f14d199fac36b0d99824
yisak-67 <yisakzemedu6774@gmail.com>
2024-10-29 11:40:05 +0300

    update setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './ui/Layout.jsx'
+import { createBrowserRouter, Outlet, RouterProvider, ScrollRestoration } from 'react-router-dom'
 
+const RouterLayout = () => {
+
+  return (
+    <Layout>
+       <ScrollRestoration />
+       <Outlet />
+
+    </Layout>
+
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RouterLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+    
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<RouterProvider router={router} />
 )
