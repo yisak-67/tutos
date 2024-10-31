9145e9f5d1462a553036e9dfe694802a9e3ac4ed
yisak-67 <yisakzemedu6774@gmail.com>
2024-10-31 21:05:59 +0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+
 import "react-multi-carousel/lib/styles.css";
-import Header from './ui/Header'
+
 import BannerCategories from './ui/BAnnerCatagory'
 import HomeBanner from './ui/HomeBanner';
 import Hightlight from './ui/Hightlight';
@@ -8,7 +8,7 @@ import Hightlight from './ui/Hightlight';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <main>
